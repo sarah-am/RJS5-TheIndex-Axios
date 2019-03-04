2929d09ac219a9d6cba43e33d2e4d2176fadcc05
sarah-am <sarahq.aman@gmail.com>
2019-03-04 20:15:22 +0300

    Nour and Sarah

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
 import React, { Component } from "react";
 
-import authors from "./data.js";
+// import authors from "./data.js";
+import axios from "axios";
 
 // Components
 import Sidebar from "./Sidebar";
 import AuthorsList from "./AuthorsList";
 import AuthorDetail from "./AuthorDetail";
+import Loading from "./Loading";
 
 class App extends Component {
   state = {
     currentAuthor: null,
-    filteredAuthors: authors
+    filteredAuthors: [],
+    authors: [],
+    loading: true
   };
 
-  selectAuthor = author => this.setState({ currentAuthor: author });
+  selectAuthor = async author => {
+    this.setState({ loading: true });
+    let response = await axios.get(
+      `https://the-index-api.herokuapp.com/api/authors/${author.id}/`
+    );
+    this.setState({ currentAuthor: response.data });
+    this.setState({ loading: false });
+  };
 
   unselectAuthor = () => this.setState({ currentAuthor: null });
 
   filterAuthors = query => {
     query = query.toLowerCase();
-    let filteredAuthors = authors.filter(author => {
+    let filteredAuthors = this.state.authors.filter(author => {
       return `${author.first_name} ${author.last_name}`
         .toLowerCase()
         .includes(query);
@@ -28,18 +39,33 @@ class App extends Component {
   };
 
   getContentView = () => {
-    if (this.state.currentAuthor) {
+    if (this.state.loading) {
+      return <Loading />;
+    } else if (this.state.currentAuthor) {
       return <AuthorDetail author={this.state.currentAuthor} />;
     } else {
       return (
         <AuthorsList
-          authors={this.state.filteredAuthors}
+          authors={this.state.authors}
           selectAuthor={this.selectAuthor}
           filterAuthors={this.filterAuthors}
         />
       );
     }
   };
+
+  async componentDidMount() {
+    try {
+      let response = await axios.get(
+        "https://the-index-api.herokuapp.com/api/authors/"
+      );
+      let authorsAPI = response.data;
+      this.setState({ authors: authorsAPI, loading: false });
+      // this.setState({ loading: false });
+    } catch (error) {
+      console.error("Something Went Wrong");
+    }
+  }
 
   render() {
     return (
--- src/BookRow.js
+++ src/BookRow.js
@@ -3,13 +3,14 @@ import React, { Component } from "react";
 class BookRow extends Component {
   render() {
     const book = this.props.book;
-    const author = this.props.author;
+    // const author = this.props.author;
+
+    const bookAuthors = book.authors.map(author => author.name + ", ");
+
     return (
       <tr>
         <td>{book.title}</td>
-        <td>
-          {author.first_name} {author.last_name}
-        </td>
+        <td>{bookAuthors}</td>
         <td>
           <button className="btn" style={{ backgroundColor: book.color }} />
         </td>
